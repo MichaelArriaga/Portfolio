47e104528d03fb024b9e29f25a55d48a870af763
michaelarriaga <m.arriaga.smb@gmail.com>
2018-12-24 21:27:36 -0800

    Add some markup boilerplate to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,14 +4,21 @@ import React from 'react';
 import Header from '../components/header';
 import ResponsiveHeader from '../components/responsive-header';
 
+// page components
 
+// styling
 import styles from './contact-css.module.css';
 
 
-export default () => (
-  <div>
-    <ResponsiveHeader />
-    <Header />
-    <h1 className={styles.contactClass}>Contact page RED</h1>
-  </div>
-)
+  export default () => (
+    <div>
+      <ResponsiveHeader />
+      <Header />
+      <div className={styles.contactContainer}>
+        <h1 className={styles.contactHeading}>/CONTACT</h1>
+
+      </div>
+      
+    </div>
+
+  )
