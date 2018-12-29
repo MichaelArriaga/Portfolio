73065ac76adedd90f94374741e4dff7a8ae9d0fc
michaelarriaga <m.arriaga.smb@gmail.com>
2018-12-29 10:30:12 -0800

    Improve footer responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,18 +7,16 @@ import githubicon from '../../static/etc/images/github-icon.svg';
 
 export default () => (
     <div className={styles.footerContainer}>
+    
         <hr className={styles.footerLine}></hr>
-        <p className={styles.footerText}>Design + Code by <a className={styles.link} href="https://twitter.com/ux_Michael" target="_blank" rel="noopener noreferrer">Mike Arriaga</a> in Seattle, WA</p>
-        <ul className={styles.footerIconList}>
 
-            <li className={styles.footerIcon}>
-            <a href="https://twitter.com/ux_Michael" target="_blank" rel="noopener noreferrer"><img src={twittericon} alt="not loaded" /></a>
-            </li>
+        <p className={styles.footerText}>Design + Code by <a className={styles.link} href="https://twitter.com/ux_Michael" target="_blank" rel="noopener noreferrer">Mike Arriaga</a> in Seattle, WA | 
+        
+            <a className={styles.footerIcon}href="https://twitter.com/ux_Michael" target="_blank" rel="noopener noreferrer"><img src={twittericon} alt="not loaded" /></a>
 
-            <li className={styles.footerIcon}>
-            <a href="https://github.com/MichaelArriaga" target="_blank" rel="noopener noreferrer"><img src={githubicon} alt="not loaded" /></a>
-            </li>
+            <a className={styles.footerIcon}href="https://github.com/MichaelArriaga" target="_blank" rel="noopener noreferrer"><img src={githubicon} alt="not loaded" /></a> 
 
-        </ul>
+        </p>
+
     </div>
 )
