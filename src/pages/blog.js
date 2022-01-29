2548bdb89cc2c655809882b01fe7e1223483f73d
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-01-29 14:19:30 -0800

    show posts list on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,12 @@
 import React from 'react';
 // page components
-import PostList from '../components/PostList';
-import Post from '../components/Post';
-
+import Post from '../components/index/Post';
 // styling
 import HeroHeader from '../components/general/HeroHeader';
 import ReusableHelmet from '../components/ReusableHelmet';
 import SideBar from '../components/Sidebar';
 import fonts from '../constants/font_names';
-import { Link } from 'gatsby';
+import { Link, graphql, StaticQuery } from 'gatsby';
 import Footer from '../components/Footer';
 import aboutimg from '../../static/etc/images/pages/about/self-pic.png';
 
@@ -42,12 +40,104 @@ const Blog = () => (
           >
             /Posts
           </h2>
-          <p
-            style={{ fontFamily: fonts.regular }}
-            className={'text-lg sm:text-xl mb-2 text-gray-900'}
-          >
-            Post list
-          </p>
+          {/* post lists start */}
+          <StaticQuery
+            query={graphql`
+          query {
+            allMarkdownRemark(
+              sort: { order: DESC, fields: [frontmatter___date] }
+              limit: 100
+            ) {
+              edges {
+                node {
+                  id
+                  excerpt(pruneLength: 250)
+                  frontmatter {
+                    date(formatString: "MMMM DD, YYYY")
+                    slug
+                    title
+                    tags
+                  }
+                }
+              }
+            }
+          }
+        `}
+            render={(data) => {
+              const posts = data.allMarkdownRemark.edges;
+              return (
+                <>
+                  {posts.length > 0
+                    ? posts.map((item) => {
+                        let post = item.node.frontmatter;
+                        let key = post.title;
+                        let title = post.title;
+                        let date = post.date;
+                        let slug = post.slug;
+                        let post_tags = post.tags.split(',');
+
+                        return (
+                          <div key={key} className={'sm:max-w-xl '}>
+                            <Link
+                              className="flex justify-start items-center mb-1"
+                              to={slug}
+                            >
+                              <h3
+                                style={{ fontFamily: fonts.semiBold }}
+                                className={
+                                  'text-base sm:text-lg text-gray-900 text-left flex flex-wrap justify-start items-center'
+                                }
+                              >
+                                <span className="underline mr-2">{title}</span>{' '}
+                                <span
+                                  style={{ color: '#9F9F9F' }}
+                                  className="tracking-tighter"
+                                >
+                                  ({date})
+                                </span>
+                              </h3>
+                            </Link>
+
+                            <div
+                              className={
+                                'flex flex-row justify-start items-center'
+                              }
+                            >
+                              {post_tags && post_tags.length > 0
+                                ? post_tags.map((tag) => (
+                                    // post tag start
+                                    <Link
+                                      key={tag}
+                                      to={''}
+                                      style={{
+                                        paddingTop: 3,
+                                        paddingBottom: 3,
+                                        paddingLeft: 6,
+                                        paddingRight: 6,
+                                        backgroundColor: '#A0EDE4',
+                                      }}
+                                      className="px-1 rounded bg-cyan-400 mr-2 mb-6"
+                                    >
+                                      <h3
+                                        style={{ fontFamily: fonts.semiBold }}
+                                        className="text-xs text-gray-900"
+                                      >
+                                        {tag}
+                                      </h3>
+                                    </Link>
+                                    // post tag end
+                                  ))
+                                : null}
+                            </div>
+                          </div>
+                        );
+                      })
+                    : null}
+                </>
+              );
+            }}
+          />
+          {/* post lists end */}
         </div>
       </div>
       {/* right panel end */}
