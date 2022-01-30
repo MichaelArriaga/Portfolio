d045e66ed2fb2346b4eb9d4be1bb7c1db09e90f2
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-01-29 20:58:10 -0800

    clean up warnings

--- src/components/general/Bold.js
+++ src/components/general/Bold.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext, useRef } from 'react';
+import React from 'react';
 import fonts from '../../constants/font_names';
 
 const Bold = ({ children }) => {
