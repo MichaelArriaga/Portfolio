3b666b6de9daa88d1aa9bb5fed816d3614e00ddf
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-04-20 16:16:20 -0700

    fix default image og:image url

--- src/components/ReusableHelmet.tsx
+++ src/components/ReusableHelmet.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import defaultImage from "../../static/etc/images/default-image.png";
 
+const env_siteUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://mike-ray.netlify.app";
+
 const ReusableHelmet = ({ title, description }: any) => {
+  const imageUrl = `${env_siteUrl}/${defaultImage}`;
+  console.log("imageUrl:", imageUrl);
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta property="og:image" content={defaultImage} />
-      <meta name="twitter:image" content={defaultImage} />
+      <meta property="og:image" content={imageUrl} />
+      <meta name="twitter:image" content={imageUrl} />
     </Helmet>
   );
 };
