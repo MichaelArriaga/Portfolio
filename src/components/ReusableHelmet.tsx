c1b14a8756ee86e779b5e9db613ad62b9f0b23e1
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-04-19 19:16:07 -0700

    add default image to non post pages

--- src/components/ReusableHelmet.tsx
+++ src/components/ReusableHelmet.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
+import defaultImage from "../../static/etc/images/default-image.png";
 
-const ReusableHelmet = ({ title, description }) => {
+const ReusableHelmet = ({ title, description }: any) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta property="og:image" content={defaultImage} />
+      <meta name="twitter:image" content={defaultImage} />
     </Helmet>
   );
 };
