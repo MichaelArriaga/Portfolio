ae4b2131737c8341c54cee76d1e8f97506c8e0ea
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-04-20 17:34:51 -0700

    add twitter:card meta tag

--- src/components/ReusableHelmet.tsx
+++ src/components/ReusableHelmet.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import defaultImage from "../../static/etc/images/default-image.png";
 
-const env_siteUrl =
+const envSiteBaseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
     : "https://mike-ray.netlify.app";
 
 const ReusableHelmet = ({ title, description }: any) => {
-  const imageUrl = `${env_siteUrl}${defaultImage}`;
-  console.log("imageUrl:", imageUrl);
+  const imageUrl = `${envSiteBaseUrl}${defaultImage}`;
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:image" content={imageUrl} />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content={imageUrl} />
     </Helmet>
   );
