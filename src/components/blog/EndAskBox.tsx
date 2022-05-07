101c282e936ada49dbac32aa293d584cb8cf8af6
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2022-05-07 15:35:21 -0700

    tweak styles of EndAskBox

--- src/components/blog/EndAskBox.tsx
+++ src/components/blog/EndAskBox.tsx
@@ -8,13 +8,13 @@ import {
 
 const EndAskBox = () => {
   return (
-    <div className="w-full bg-white rounded p-4 rounded border border-gray-800 mb-6">
+    <div className="w-full rounded p-4 mb-4">
       <div className="mb-4">
         <h1 className="text-2xl sm:text-3xl text-gray-900 font-black text-center leading-none mb-4 antialiased">
           Thanks for reading! 👋
         </h1>
         <h2 className="text-xl sm:text-2xl text-gray-900 font-bold text-center leading-none mb-4 sm:text-center antialiased">
-          Find this article useful?
+          Did you find this article useful?
         </h2>
       </div>
       <div className="flex flex-row justify-center items-center">
