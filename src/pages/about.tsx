685eccccf68d6edcfc33dbec3f49694c9a73162d
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-02-22 14:27:52 -0800

    improve about me wording and styles

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -19,26 +19,24 @@ class About extends React.Component {
           article={false}
         />
         <div style={{ marginBottom: 150 }} className="">
-          <h3 className="mb-2 text-3xl font-black">Bio</h3>
+          <h3 className="mb-2 text-3xl font-black">About Mike</h3>
 
-          <p className={"text-lg mb-3 text-gray-900 font-medium antialiased"}>
-            Mike is an indie Rails Developer based in Seattle WA. After hours, he's a Musician (Drums & Bass Guitar).
+          <p className={"text-xl mb-3 text-gray-900 font-medium antialiased"}>
+            Mike is an indie Rails Developer based in Seattle WA. After hours,
+            he's a Musician (Drums & Bass Guitar).
           </p>
-          <p className={"text-lg mb-3 text-gray-900 font-medium antialiased"}>
-            Previously, Mike was Founder, CTO and Principal Engineer at{" "}
+          <p className={"text-xl mb-3 text-gray-900 font-medium antialiased"}>
+            Mike was the Founder, CTO, and Principal Engineer of{" "}
             <Link
               to="/projects/fresh_and_tumble"
               className="antialiased font-bold underline"
             >
               Fresh And Tumble,
             </Link>{" "}
-            an Uber-of Delivery App which consisted of 2 React Native Apps:
-            (Customer/Driver) & Web App/API powered by a Rails 6 Monolith.
+            an Uber-like delivery app that included 2 React Native apps
+            (Customer and Driver) and a Rails 6 Monolith-powered web app/API.
           </p>
-          <p
-            className={"text-lg mb-3 text-gray-900 font-medium antialiased"}
-          ></p>
-          <p className={"text-lg mb-3 text-gray-900 font-medium antialiased"}>
+          <p className={"text-xl mb-3 text-gray-900 font-medium antialiased"}>
             Mike's latest product{" "}
             <a
               href="https://mintysnippets.com"
@@ -59,12 +57,18 @@ class About extends React.Component {
             </a>{" "}
           </p>
 
-          <p className={"text-lg mb-3 text-gray-900 font-medium antialiased"}>
-            With over 10 years experience in Web Development and 6 years
-            experience building Web Apps and RESTful APIs with Rails, Mike has a
-            strong understanding of both Web Development and Rails best practices.
+          <p className={"text-xl mb-3 text-gray-900 font-medium antialiased"}>
+            Mike has over 10 years of experience in web development, with a
+            specific focus on building web apps and RESTful APIs using Rails for
+            over 6 years.
           </p>
-          <p className={"text-lg mb-3 text-gray-900 font-medium antialiased"}>
+
+          <p className={"text-xl mb-3 text-gray-900 font-medium antialiased"}>
+            This has given him a solid grasp of web application development and
+            scaling best practices with Rails.
+          </p>
+
+          <p className={"text-xl mb-3 text-gray-900 font-medium antialiased"}>
             Need help with your team or project?{" "}
             <a
               href="mailto:m.arriaga.smb@gmail.com"
