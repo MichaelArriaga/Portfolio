91e02ba11093df15878833b20a4cb1460572a362
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-03-01 07:06:09 -0800

    add external/internal link components

--- src/components/index/ProjectItem.tsx
+++ src/components/index/ProjectItem.tsx
@@ -9,6 +9,12 @@ type PropTypes = {
   latest?: boolean;
 };
 
+type LinkTypes = {
+  to: string;
+  name: string;
+  latest?: boolean;
+}
+
 const ProjectItem: React.FC<PropTypes> = ({
   name,
   technologies,
@@ -18,23 +24,11 @@ const ProjectItem: React.FC<PropTypes> = ({
 }): JSX.Element => {
   return (
     <div className={"sm:max-w-xl mx-auto mb-3"}>
-      <Link
-        className="relative flex items-center justify-start mb-1"
-        to={to}
-        target={externalLink ? "_blank" : ""}
-        rel={externalLink ? "externalLink" : ""}
-      >
-        <h3
-          style={{ lineHeight: 1 }}
-          className={
-            "text-xl text-gray-900 text-left flex flex-wrap justify-start items-center font-black underline antialiased mb-1"
-          }
-        >
-          <span className="mr-2">{name}</span>
-        </h3>
-
-        {latest ? <NewTag /> : null}
-      </Link>
+      {externalLink ? (
+        <ExternalLink to={to} name={name} latest={latest} />
+      ) : (
+        <InternalLink to={to} name={name} latest={latest} />
+      )}
 
       <div className={"flex flex-row justify-start items-center "}>
         <div className="flex flex-row justify-start items-center">
@@ -53,7 +47,46 @@ const ProjectItem: React.FC<PropTypes> = ({
 
 export default ProjectItem;
 
-const NewTag = () => {
+const ExternalLink: React.FC<LinkTypes> = ({ to, name, latest }) => {
+  return (
+    <a
+      href={to}
+      className="relative flex items-center justify-start mb-1"
+      target={"_blank"}
+      rel={"noreferer"}
+    >
+      <h3
+        style={{ lineHeight: 1 }}
+        className={
+          "text-xl text-gray-900 text-left flex flex-wrap justify-start items-center font-black underline antialiased mb-1"
+        }
+      >
+        <span className="mr-2">{name}</span>
+      </h3>
+
+      {latest ? <NewTag /> : null}
+    </a>
+  );
+};
+
+const InternalLink: React.FC<LinkTypes> = ({ to, name, latest }): JSX.Element => {
+  return (
+    <Link to={to} className="relative flex items-center justify-start mb-1">
+      <h3
+        style={{ lineHeight: 1 }}
+        className={
+          "text-xl text-gray-900 text-left flex flex-wrap justify-start items-center font-black underline antialiased mb-1"
+        }
+      >
+        <span className="mr-2">{name}</span>
+      </h3>
+
+      {latest ? <NewTag /> : null}
+    </Link>
+  );
+};
+
+const NewTag: React.FC = (): JSX.Element => {
   return (
     <h3
       style={{
@@ -67,7 +100,7 @@ const NewTag = () => {
         right: 120,
         bottom: 25,
       }}
-      className="cursor-default text-xs antialiased font-bold text-white px-1 mr-1 rounded bg-gray-800"
+      className="cursor-default tracking-tighter text-xs antialiased font-black text-white px-1 mr-1 rounded bg-gray-800"
     >
       NEW!
     </h3>
